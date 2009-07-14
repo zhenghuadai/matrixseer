00a2647b18f576680c2af40f88beb2f056737769
djx.zhenghua <djx.zhenghua@f676e73a-0bc1-11de-b17c-0f1ef23b492c>
2009-07-14 02:23:36 +0000

    add .dep

--- trunk/include/rules.mk
+++ trunk/include/rules.mk
@@ -1,18 +1,44 @@
 ${DBG_DIR}/%.o:%.c
-	${CC}  ${MARC}  ${CFLAG}  ${INCLUDEPATH} -c -o $@ $<
+	${CC}  ${MARC}  ${CFLAG}  ${INCLUDEPATH}  -MMD -c -o $@ $<
+	@cp $(DBG_DIR)/$*.d $(DEP_DIR)/$*.dep; \
+		sed -e 's/#.*//' -e 's/^[^:]*: *//' -e 's/ *\\$$//' \
+			-e '/^$$/ d' -e 's/$$/ :/' < $(DBG_DIR)/$*.d >> \
+			$(DEP_DIR)/$*.dep; \
+		rm -f $(DBG_DIR)/$*.d
+
 ${DBG_DIR}/%.o:%.cpp
-	${CXX} ${MARC}  ${CFLAG}  ${INCLUDEPATH} -c -o $@ $<
+	${CXX} ${MARC}  ${CFLAG}  ${INCLUDEPATH}  -MMD -c -o $@ $<
+	@cp $(DBG_DIR)/$*.d $(DEP_DIR)/$*.dep; \
+		sed -e 's/#.*//' -e 's/^[^:]*: *//' -e 's/ *\\$$//' \
+			-e '/^$$/ d' -e 's/$$/ :/' < $(DBG_DIR)/$*.d >> \
+			$(DEP_DIR)/$*.dep; \
+		rm -f $(DBG_DIR)/$*.d
+
 ${OPT_DIR}/%.o:%.c
-	${CC}  ${MARC}  ${CFLAG}  ${INCLUDEPATH} -c -o $@ $<
+	${CC}  ${MARC}  ${CFLAG}  ${INCLUDEPATH}  -MMD -c -o $@ $<
+	@cp $(OPT_DIR)/$*.d $(DEP_DIR)/$*.dep; \
+		sed -e 's/#.*//' -e 's/^[^:]*: *//' -e 's/ *\\$$//' \
+			-e '/^$$/ d' -e 's/$$/ :/' < $(OPT_DIR)/$*.d >> \
+			$(DEP_DIR)/$*.dep; \
+		rm -f $(OPT_DIR)/$*.d
+
 ${OPT_DIR}/%.o:%.cpp
-	${CXX} ${MARC}  ${CFLAG}  ${INCLUDEPATH} -c -o $@ $<
+	${CXX} ${MARC}  ${CFLAG}  ${INCLUDEPATH}  -MMD -c -o $@ $<
+	@cp $(OPT_DIR)/$*.d $(DEP_DIR)/$*.dep; \
+		sed -e 's/#.*//' -e 's/^[^:]*: *//' -e 's/ *\\$$//' \
+			-e '/^$$/ d' -e 's/$$/ :/' < $(OPT_DIR)/$*.d >> \
+			$(DEP_DIR)/$*.dep; \
+		rm -f $(OPT_DIR)/$*.d
+
 %.o:%.c
-	${CC}  ${MARC}  ${CFLAG}  ${INCLUDEPATH} -c -o $@ $<
+	${CC}  ${MARC}  ${CFLAG}  ${INCLUDEPATH}  -MMD -c -o $@ $<
 %.o:%.cpp
-	${CXX} ${MARC}  ${CFLAG}  ${INCLUDEPATH} -c -o $@ $<
+	${CXX} ${MARC}  ${CFLAG}  ${INCLUDEPATH}  -MMD -c -o $@ $<
 ${LIBPATH}:
 	@mkdir -p $@
+-include ${DEP_FILES} 
 createDir:
 	@test -x ${DBG_DIR} || mkdir -p ${DBG_DIR}
+	@test -x ${DEP_DIR} || mkdir -p ${DEP_DIR}
 clean:
 	rm -rf ${OBJS} ${CLEAN_OBJS} 
