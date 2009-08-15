25bc40dbe9d4cae2806c34172a48d749d47b9c2e
djx.zhenghua <djx.zhenghua@f676e73a-0bc1-11de-b17c-0f1ef23b492c>
2009-08-15 02:38:28 +0000

    refactory display

--- trunk/src/glutDisplay/display.h
+++ trunk/src/glutDisplay/display.h
@@ -92,5 +92,6 @@ int putStrScr(const char *content,int x,int y,int w,int h,int offsetx);
 int2 getWH_Chars(int w,int h,int id);
 int getWinw();
 int getWinh();
+int getRowHeight();
 #define GM_BLACK 0
 #endif
