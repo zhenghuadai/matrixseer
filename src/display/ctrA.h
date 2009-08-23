5bb158fbcf51be91f816c070b75ebb27a7b6a5b0
djx.zhenghua <djx.zhenghua@f676e73a-0bc1-11de-b17c-0f1ef23b492c>
2009-08-23 13:02:08 +0000

    ctrA

--- src/display/ctrA.h
+++ src/display/ctrA.h
@@ -7,31 +7,32 @@
 #include <stdio.h>
 #include "dmGlobal.h"
 #include "Box.h"
-class ctrA : public Widget {
+#include "ctrText.h"
+class ctrA : public ctrText{
     int handle(int);
     public:
     int pushed ;
     void draw(){};
     ctrA(Box* b,int w,int h,const char *l)
-        : Widget(0,0,w,h,l) 
+        : ctrText(0,0,w,h,l) 
     {box(b);pushed = 0;};
 
     ctrA(int w,int h,const char *l)
-        : Widget(0,0,w,h,l)
+        : ctrText(0,0,w,h,l)
     {box(BOX_BODER_SHAPE);pushed = 0;}
 
     ctrA(int w,int h)
-        : Widget(0,0,w,h,(const char *)0) 
+        :ctrText (0,0,w,h,(const char *)0) 
     {box(BOX_BODER_SHAPE);pushed = 0;}
 
     ctrA(int x,int y,int w,int h,const char *l)
-        : Widget(x,y,w,h,l) 
+        : ctrText(x,y,w,h,l) 
     {box(BOX_BODER_SHAPE);pushed = 0; 
         //labelsize(16);
     }
 
     ctrA(Box* b,int x,int y,int w,int h,const char *l)
-        : Widget(x,y,w,h,l) 
+        : ctrText(x,y,w,h,l) 
     {}
 };
 
