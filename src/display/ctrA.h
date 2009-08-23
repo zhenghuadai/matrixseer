f4eab781ade323cfcb3872ecb2f8c6b7caae95db
djx.zhenghua <djx.zhenghua@f676e73a-0bc1-11de-b17c-0f1ef23b492c>
2009-08-23 13:25:20 +0000

    format

--- src/display/ctrA.h
+++ src/display/ctrA.h
@@ -9,31 +9,32 @@
 #include "Box.h"
 #include "ctrText.h"
 class ctrA : public ctrText{
-    int handle(int);
     public:
-    int pushed ;
-    void draw(){};
-    ctrA(Box* b,int w,int h,const char *l)
-        : ctrText(0,0,w,h,l) 
-    {box(b);pushed = 0;};
+        int handle(int);
+    public:
+        int pushed ;
+        void draw(){};
+        ctrA(Box* b,int w,int h,const char *l)
+            : ctrText(0,0,w,h,l) 
+        {box(b);pushed = 0;};
 
-    ctrA(int w,int h,const char *l)
-        : ctrText(0,0,w,h,l)
-    {box(BOX_BODER_SHAPE);pushed = 0;}
+        ctrA(int w,int h,const char *l)
+            : ctrText(0,0,w,h,l)
+        {box(BOX_BODER_SHAPE);pushed = 0;}
 
-    ctrA(int w,int h)
-        :ctrText (0,0,w,h,(const char *)0) 
-    {box(BOX_BODER_SHAPE);pushed = 0;}
+        ctrA(int w,int h)
+            :ctrText (0,0,w,h,(const char *)0) 
+        {box(BOX_BODER_SHAPE);pushed = 0;}
 
-    ctrA(int x,int y,int w,int h,const char *l)
-        : ctrText(x,y,w,h,l) 
-    {box(BOX_BODER_SHAPE);pushed = 0; 
-        //labelsize(16);
-    }
+        ctrA(int x,int y,int w,int h,const char *l)
+            : ctrText(x,y,w,h,l) 
+        {box(BOX_BODER_SHAPE);pushed = 0; 
+            //labelsize(16);
+        }
 
-    ctrA(Box* b,int x,int y,int w,int h,const char *l)
-        : ctrText(x,y,w,h,l) 
-    {}
+        ctrA(Box* b,int x,int y,int w,int h,const char *l)
+            : ctrText(x,y,w,h,l) 
+        {}
 };
 
 #endif
